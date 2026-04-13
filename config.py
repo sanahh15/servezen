cc2af68594ae6d71ca3ad33f01d4b97b11b07e84
sanahh15 <sahanar2065@gmail.com>
2026-04-13 17:42:45 +0530

    fixed config class

--- config.py
+++ config.py
@@ -3,12 +3,23 @@
 #     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/servezen"
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+#import os
+
+#db_url = os.getenv("DATABASE_URL")
+
+#if db_url and db_url.startswith("postgres://"):
+ #   db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+#SQLALCHEMY_DATABASE_URI = db_url
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 import os
 
-db_url = os.getenv("DATABASE_URL")
+class Config:
+    db_url = os.getenv("DATABASE_URL")
 
-if db_url and db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-SQLALCHEMY_DATABASE_URI = db_url
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = db_url
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
