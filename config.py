7080c609d59fa809c36265641ab2cd59bee8ecbc
sanahh15 <sahanar2065@gmail.com>
2026-04-13 17:33:25 +0530

    fix postgres connection

--- config.py
+++ config.py
@@ -5,5 +5,10 @@
 
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = db_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
